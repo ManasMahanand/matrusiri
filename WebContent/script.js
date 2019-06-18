e75f8b8c00ecc55df4939e4c2915a8463ed51e5a
Manas Mahanand <robot14.mahanand@gmail.com>
2019-06-18 19:29:21 +0530

    added src code

---
+++ WebContent/script.js
@@ -0,0 +1,16 @@
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".carousel-control-prev").click(function(){
+  $("#myCarousel").carousel("prev");
+});
+
+$('#myCarousel').carousel({
+  interval: 1000
+});
